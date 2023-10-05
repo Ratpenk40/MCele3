7ef8872b4539524dc30013cf352397c220b40e0c
Ratpenk40 <alexandre.vaudano@gmail.com>
2023-10-05 14:41:02 +0200

    lim 0,30

--- python/module_2Dplotter_PLK1.py
+++ python/module_2Dplotter_PLK1.py
@@ -183,14 +183,14 @@ class Plot2D:
         fig_id1.colorbar(im2)
 
         self.ax_id3 = fig_id1.add_subplot(2, 2, 4)
-        hist3, xbins3, ybins3, im3 = self.ax_id3.hist2d(x4, y4, bins=self.limits[0][1], range=self.limits)
+        hist3, xbins3, ybins3, im3 = self.ax_id3.hist2d(x4, y4, bins=self.limits[0][1], range=[[0,50], [0,30]])
         self.ax_id3.set_title("PLK-1")
         self.ax_id3.set_xlabel("Long axis (um)")
         self.ax_id3.set_ylabel("Short axis (um)")
         self.ax_id3.set_ylim(bottom=0, top=30)
 
         fig_id1.colorbar(im3)
-
+        
         self.data_movie_1 = hist3.T
 
         density = self.limits[0][1] / self.limits[0][1]
